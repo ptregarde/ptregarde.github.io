c2e185c73e54b0bbd467f4309e667e40945a17e1
Patricia <ptregarde@gmail.com>
2019-01-28 17:34:42 +0800

    Started first three sections

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+;(function($) {
+  'use strict';
+
+  var $window  = $(window);
+
+  function sectionWaypoints() {
+    if ($window.width() >= 960) {
+      $('.benefit').waypoint(function(direction) {
+        if (direction === 'down') {
+          $(this.element)
+          .removeClass('fadeOut')
+          .addClass('fadeIn')
+          .find('.heading, .description')
+          .show();
+        }
+      }, { offset: '50%' })
+      $('.benefit').waypoint(function(direction) {
+        if (direction === 'up') {
+          $(this.element)
+          .removeClass('fadeIn')
+          .addClass('fadeOut')
+          .find('.heading, .description')
+          .delay(500)
+          .hide(0);
+        }
+      }, { offset: '20%' });
+    }
+  }
+
+  // waypoints
+  sectionWaypoints();
+  $window.resize(function() {
+    sectionWaypoints();
+  });
+
+})(jQuery);
+
